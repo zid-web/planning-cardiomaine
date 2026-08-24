679ea935442ec63fa84c33af9b6179b1c1ccf960
zid-web <ouassim34@yahoo.fr>
2026-08-24 15:14:36 +0200

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -85,6 +85,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|sw.js|manifest.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 }
