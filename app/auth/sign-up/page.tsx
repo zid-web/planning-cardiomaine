807df6f1c10fe8ec1391984151ddd8b1e4ef2005
zid-web <ouassim34@yahoo.fr>
2026-09-08 15:51:39 +0200

    Adjust layout for sign-up page

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -76,7 +76,7 @@ export default function Page() {
   }
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex h-svh w-full items-center justify-center overflow-y-auto p-6 md:p-10">
       <div className="w-full max-w-sm">
         <div className="flex flex-col gap-6">
           <Card>
